6073e527910b103a35bf4abd22ad058695d0137f
honzikschenk <nojschenk@gmail.com>
2025-02-24 22:41:48 -0500

    Update Footnote component SVG className and add smooth scroll behavior in CSS

--- src/components/Footnote.tsx
+++ src/components/Footnote.tsx
@@ -33,7 +33,7 @@ const Footnote = () => {
             <FaLinkedin size={32} />
           </a>
           <a href="https://se-webring.xyz" target="_blank" rel="noopener noreferrer" className="text-foreground transition duration-750 hover:text-orange-500">
-            <svg class="w-8 h-8" viewBox="0 0 960 960" fill="none" xmlns="http://www.w3.org/2000/svg"> <rect width="960" height="960" fill="none"></rect> <path fill-rule="evenodd" clip-rule="evenodd" d="M369.6,223.6v512.5c124.9-18.2,220.9-126,220.9-256.2s-96-238.1-220.9-256.2ZM341.1,812.6c-3,0-5.9.1-8.9.1C148.7,812.7,0,663.7,0,479.8S148.7,146.9,332.1,146.9s6,0,8.9.1c1.1,0,2.3-.1,3.4-.1h615.5v665.9H344.5c-1.1,0-2.3,0-3.4-.1ZM295.8,736.2V223.4c-125.4,17.7-222,125.8-222,256.4s96.5,238.7,222,256.4ZM540.9,220.9h345.3v517.9h-345.3c75.2-61,123.4-154.4,123.4-258.9s-48.1-197.9-123.4-258.9Z" fill="currentColor"></path> </svg>
+            <svg className="w-8 h-8" viewBox="0 0 960 960" fill="none" xmlns="http://www.w3.org/2000/svg"> <rect width="960" height="960" fill="none"></rect> <path fill-rule="evenodd" clip-rule="evenodd" d="M369.6,223.6v512.5c124.9-18.2,220.9-126,220.9-256.2s-96-238.1-220.9-256.2ZM341.1,812.6c-3,0-5.9.1-8.9.1C148.7,812.7,0,663.7,0,479.8S148.7,146.9,332.1,146.9s6,0,8.9.1c1.1,0,2.3-.1,3.4-.1h615.5v665.9H344.5c-1.1,0-2.3,0-3.4-.1ZM295.8,736.2V223.4c-125.4,17.7-222,125.8-222,256.4s96.5,238.7,222,256.4ZM540.9,220.9h345.3v517.9h-345.3c75.2-61,123.4-154.4,123.4-258.9s-48.1-197.9-123.4-258.9Z" fill="currentColor"></path> </svg>
           </a>
         </motion.div>
         <motion.p
